6673364d7337e1fc993a1d43bb530d3840376f2f
Abdullah Asim <abdullahshaggy@gmail.com>
2024-05-25 22:40:51 +0500

    Student Panel skeleton done

--- src/screens/student/StudentPanel.js
+++ src/screens/student/StudentPanel.js
@@ -1,6 +1,39 @@
 import GlobalStyleSheet from '../../../GlobalStyleSheet';
 
-const StudentPanel = ({navigation}) => {
+import React, { useState } from 'react';
+import { View } from 'react-native';
+import { Title, TextInput, Button } from 'react-native-paper';
 
+const StudentPanel = ({navigation}) => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    return(
+        <View style={GlobalStyleSheet.container}>
+      <Title>Student Login</Title>
+      <TextInput
+        label="Email"
+        value={email}
+        onChangeText={setEmail}
+        style={GlobalStyleSheet.myInput}
+      />
+      <TextInput
+        label="Password"
+        value={password}
+        onChangeText={setPassword}
+        secureTextEntry
+        style={GlobalStyleSheet.myInput}
+      />
+      <Button 
+      
+      mode='elevated'
+      buttonColor='#875FF6'
+      textColor='white'
+        onPress={() => navigation.navigate('StudentDashboard')} 
+        style={GlobalStyleSheet.myButton}>
+        Login
+        </Button>
+    </View>
+    );
 }
 export default StudentPanel
